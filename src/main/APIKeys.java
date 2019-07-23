3997338a8070fa6ddfccf794d7e73d4f75c5ab34
Zed Chance <zchance@student.sierracollege.edu>
2019-07-22 20:27:41 -0700

    Added APIKeys with keys for NHL and MLB

---
+++ src/main/APIKeys.java
@@ -0,0 +1,8 @@
+package main;
+
+public class APIKeys
+{
+   // Sportradar.com API keys
+   final static String MLB_KEY = "k6rdxtz2c98jw4awwmv3xuqh";
+   final static String NHL_KEY = "28uwvqk47m9p528gee36nx9q";
+}
